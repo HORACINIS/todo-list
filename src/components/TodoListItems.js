e34129c75e26c4fd99ed62e0c31bd43bca5a728c
horacinis <horacinis@gmail.com>
2021-04-09 18:51:48 +1000

    Installed UUID and assign it to keys for TodoListItems created

--- src/components/TodoListItems.js
+++ src/components/TodoListItems.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const TodoListItems = ({ todoItems }) => {
 
   return (
     <ul>
-      {todoItems.map((item, index) => {
+      {todoItems.map((item) => {
         return (
-          <TodoListItem key={index} {...item} />
+          <TodoListItem key={uuidv4()} {...item} />
         )
       })}
     </ul>
