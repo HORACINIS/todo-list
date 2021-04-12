6b9f3485f410213b617d9642aa4ac28512a5032d
horacinis <horacinis@gmail.com>
2021-04-12 13:06:44 +1000

    Did a bit of refactoring

--- src/components/messages/MainMessage.js
+++ src/components/messages/MainMessage.js
@@ -1,27 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 const MainMessage = ({ todoItems, radioBtnSelected: { value } }) => {
+  const [messageToDisplay, setMessageToDisplay] = useState('Create a To-do');
   const numOfItems = todoItems.length;
   const btnSelectedValue = value;
 
-  const [messageToDisplay, setMessageToDisplay] = useState('Create a To-do')
-
-  // useEffect(() => {
-  //   console.log(todoItems)
-  //   console.log(value)
-  // }, [value, todoItems])
 
   useEffect(() => {
-    if (btnSelectedValue === '0' && numOfItems === 0) setMessageToDisplay('Create a To-do')
-    if (btnSelectedValue === '1' && numOfItems === 0) setMessageToDisplay('No To-do items')
-    if (btnSelectedValue === '2' && numOfItems === 0) setMessageToDisplay('No Complete items')
+    if (numOfItems === 0) {
+      if (btnSelectedValue === '0') setMessageToDisplay('Create a To-do')
+      if (btnSelectedValue === '1') setMessageToDisplay('No To-do items')
+      if (btnSelectedValue === '2') setMessageToDisplay('No Complete items')
+    }
 
-
-
-    console.log('Num of items')
-    console.log(numOfItems)
-    console.log('Button selected value')
-    console.log(btnSelectedValue)
+    // console.log('Num of items')
+    // console.log(numOfItems)
+    // console.log('Button selected value')
+    // console.log(btnSelectedValue)
   }, [numOfItems, btnSelectedValue])
 
 
